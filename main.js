581603bbe638a309b6125562a64152a87bc50627
AvailSajahlahh <77333227+AvailSajahlahh@users.noreply.github.com>
2022-11-12 22:52:23 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function download(){
+    alert('Thanks For Using My Website');
+    alert('My Website Is Absolutely Not Phising Website');
+    alert('Donate Me For Supporting Me On Upload A Mod APK');
+    alert('Enjoy My Collection Of Mod Game:)')
+}
