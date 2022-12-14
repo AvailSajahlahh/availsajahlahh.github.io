43b365a9f70a2da77197e94a73680b1c48a538e6
AvailSajahlahh <77333227+AvailSajahlahh@users.noreply.github.com>
2022-12-14 19:42:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,3 @@ function download(){
     alert('Donate Me For Supporting Me On Upload A Mod APK');
     alert('Enjoy My Collection Of Mod Game:)')
 }
-async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3695650959314712"
-     crossorigin="anonymous"
